adcbcd051699fda47bdb43aeae131ff57dea9a09
mavo97 <mavodeveloper@gmail.com>
2020-06-08 00:36:58 -0500

    update registry design and forms

---
+++ src/app/models/alumno.model.ts
@@ -0,0 +1,13 @@
+export class AlumnoModel {
+    // tslint:disable-next-line: variable-name
+    id_usuario?: string;
+    nombre: string;
+    apellidos: string;
+    telefono: string;
+    correo: string;
+    contrasena: string;
+    // tslint:disable-next-line: variable-name
+    rol_usuario: string;
+    carrera: string;
+    no_control: string;
+}
---
+++ src/app/models/personal.model.ts
@@ -0,0 +1,14 @@
+export class PersonalModel {
+    // tslint:disable-next-line: variable-name
+    id_usuario?: string;
+    nombre: string;
+    apellidos: string;
+    telefono: string;
+    correo: string;
+    contrasena: string;
+    // tslint:disable-next-line: variable-name
+    rol_usuario: string;
+    rfc: string;
+    ocupacion: string;
+    departamento: string;
+}
